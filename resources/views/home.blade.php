78431fd33a3309d19391feab18190dbcc6f48144
Kurlok <felipe.augum@gmail.com>
2019-07-31 16:48:18 -0300

    Alteração de máscaras

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,9 @@
         $('#cep').mask('00000-000');
         $('.phone').mask('0000-0000');
         $('.phone_with_ddd').mask('(00) 0000-0000');
-        $('.tel').mask('(00) 00000-0000');
+        $('.telefone').mask('(00) 00000-0000');
+        $('.telefoneAlternativo').mask('(00) 00000-0000');
+
         $('.phone_us').mask('(000) 000-0000');
         $('.mixed').mask('AAA 000-S0S');
         $('.cpf').mask('000.000.000-00', {
